dda62bb69627f7cedadb6e3e53f5308ef7121026
Adrian Andersen <adrianandersen@protonmail.com>
2023-02-07 18:17:11 +0100

    feat: static site generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,31 @@ import {Box, CircularProgress, Container, Divider, Stack, Typography, useTheme} 
 import Head from "next/head";
 import Schedule from "../components/Schedule";
 import Config from "../components/Config";
+import {apiUrl} from "../config/config";
 import {SitSchedule} from "../types/sitTypes";
 
 // Memoize to avoid redundant schedule re-render on class selection change
 const ScheduleMemo = memo(Schedule);
 
-const Index: NextPage = () => {
+async function fetchSchedule(): Promise<SitSchedule> {
+    return await fetch(`${apiUrl}/api/schedule`, {method: 'POST'})
+        .then(res => res.json())
+}
 
-    const [schedule, setSchedule] = useState<SitSchedule | null>(null);
-    const [selectedClassIds, setSelectedClassIds] = useState<string[]>([]);
+export async function getStaticProps() {
+    const schedule = await fetchSchedule();
+    const invalidationTimeInSeconds = 60*60;
 
-    useEffect(() => {
-        fetchSchedule().then(setSchedule)
-    }, []);
-
-    async function fetchSchedule(): Promise<SitSchedule> {
-        return await fetch("/api/schedule", {method: 'POST'}).then(res => res.json())
+    return {
+        props: {
+            schedule,
+        },
+        revalidate: invalidationTimeInSeconds
     }
+}
+
+const Index: NextPage<{schedule: SitSchedule}> = ({schedule}) => {
+    const [selectedClassIds, setSelectedClassIds] = useState<string[]>([]);
 
     const classes = useMemo(() => {
         return schedule?.days.flatMap((d) => d.classes) ?? []
@@ -42,39 +50,26 @@ const Index: NextPage = () => {
                 <title>sit-rezervo/config.yaml</title>
                 <link rel="icon" href="/favicon.svg" />
             </Head>
-            {schedule ? (
-                <div>
-                    <Box height={"8vh"} display="flex" alignItems={"center"}>
-                        <Typography variant="h5" component="div" py={2} pl={3}>
-                            <strong
-                                style={{color: theme.palette.primary.main}}>sit-rezervo</strong> / <code>config.yaml</code>
-                        </Typography>
-                    </Box>
-                    <Divider/>
-                    <Stack
-                        direction={{ xs: 'column', md: 'row' }}
-                        divider={<Divider orientation="vertical" flexItem/>}
-                    >
-                        <Container maxWidth={false} sx={{height: { xs: '70vh', md: '91vh' }, overflow: 'auto'}}>
-                            <ScheduleMemo schedule={schedule} onSelectedChanged={onSelectedChanged}/>
-                        </Container>
-                        <Container sx={{paddingY: 2, height: { xs: 'auto', md: '91vh' }, overflow: 'auto', maxWidth: {xs: "100%", md: 444}}}>
-                            <Config classes={classes} selectClassIds={selectedClassIds}/>
-                        </Container>
-                    </Stack>
-
-                </div>
-            ) : (
-                <Box display={"flex"} flexDirection={"column"} alignItems={"center"} width={"100%"} marginTop={20}
-                     gap={4}>
-                    <CircularProgress/>
-                    <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-                        <Typography>Fetching schedule</Typography>
-                        <Typography sx={{opacity: 0.4}}>ibooking.sit.no</Typography>
-                    </Box>
+            <div>
+                <Box height={"8vh"} display="flex" alignItems={"center"}>
+                    <Typography variant="h5" component="div" py={2} pl={3}>
+                        <strong
+                            style={{color: theme.palette.primary.main}}>sit-rezervo</strong> / <code>config.yaml</code>
+                    </Typography>
                 </Box>
-            )
-            }
+                <Divider/>
+                <Stack
+                    direction={{ xs: 'column', md: 'row' }}
+                    divider={<Divider orientation="vertical" flexItem/>}
+                >
+                    <Container maxWidth={false} sx={{height: { xs: '70vh', md: '91vh' }, overflow: 'auto'}}>
+                        <ScheduleMemo schedule={schedule} onSelectedChanged={onSelectedChanged}/>
+                    </Container>
+                    <Container sx={{paddingY: 2, height: { xs: 'auto', md: '91vh' }, overflow: 'auto', maxWidth: {xs: "100%", md: 444}}}>
+                        <Config classes={classes} selectClassIds={selectedClassIds}/>
+                    </Container>
+                </Stack>
+            </div>
         </div>
     )
 }
