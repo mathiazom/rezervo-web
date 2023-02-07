f6089daf4f1cb3349d14a00ac9c975c05f37aa9a
Adrian Andersen <adrianandersen@protonmail.com>
2023-02-07 18:07:15 +0100

    feat: static site generation

--- config/config.ts
+++ config/config.ts
@@ -1,1 +1,3 @@
+const dev = process.env.NODE_ENV !== 'production';
+export const apiUrl = dev ? 'http://localhost:3000' : 'https://sit.biku.be';
 export const GROUP_BOOKING_URL = "https://www.sit.no/trening/gruppe"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type {NextPage} from 'next'
+<<<<<<< Updated upstream
 import React, {memo, useCallback, useEffect, useMemo, useState} from "react";
 import {Box, CircularProgress, Container, Divider, Stack, Typography, useTheme} from "@mui/material";
 import Head from "next/head";
@@ -8,9 +9,27 @@ import {SitSchedule} from "../types/sitTypes";
 
 // Memoize to avoid redundant schedule re-render on class selection change
 const ScheduleMemo = memo(Schedule);
+=======
+import React, {useState} from "react";
+import {
+    Box,
+    Container,
+    Divider,
+    Stack,
+    Typography, useTheme
+} from "@mui/material";
+import Head from "next/head";
+import Schedule from "../components/Schedule";
+import Config from "../components/Config";
+import {apiUrl} from "../config/config";
+>>>>>>> Stashed changes
 
-const Index: NextPage = () => {
+async function fetchSchedule(): Promise<any> {
+    return await fetch(`${apiUrl}/api/schedule`, {method: 'POST'})
+        .then(res => res.json())
+}
 
+<<<<<<< Updated upstream
     const [schedule, setSchedule] = useState<SitSchedule | null>(null);
     const [selectedClassIds, setSelectedClassIds] = useState<string[]>([]);
 
@@ -34,10 +53,36 @@ const Index: NextPage = () => {
         }
     }, [])
 
+=======
+export async function getStaticProps() {
+    const schedule = await fetchSchedule();
+    const invalidationTimeInSeconds = 60*60;
+
+    return {
+        props: {
+            schedule,
+        },
+        revalidate: invalidationTimeInSeconds
+    }
+}
+
+const Index: NextPage<{schedule: any}> = ({schedule}) => {
+    const [selectedClasses, setSelectedClasses] = useState<any[]>([]);
+
+    function addClass(_class: any) {
+        setSelectedClasses((classes) => [...classes, _class])
+    }
+
+    function removeClass(_class: any) {
+        setSelectedClasses((classes) => classes.filter((c) => c.id != _class.id))
+    }
+
+>>>>>>> Stashed changes
     const theme = useTheme()
 
     return (
         <div>
+<<<<<<< Updated upstream
             <Head>
                 <title>sit-rezervo/config.yaml</title>
                 <link rel="icon" href="/favicon.svg" />
@@ -72,9 +117,31 @@ const Index: NextPage = () => {
                         <Typography>Fetching schedule</Typography>
                         <Typography sx={{opacity: 0.4}}>ibooking.sit.no</Typography>
                     </Box>
+=======
+            <div>
+                <Head>
+                    <title>sit-rezervo-confgen</title>
+                </Head>
+                <Box height={"8vh"} display="flex" alignItems={"center"}>
+                    <Typography variant="h5" component="div" py={2} pl={3}>
+                        <strong style={{color: theme.palette.primary.main}}>sit-rezervo</strong> / <code>config.yaml</code>
+                    </Typography>
+>>>>>>> Stashed changes
                 </Box>
-            )
-            }
+                <Divider/>
+                <Stack
+                    direction={"row"}
+                    divider={<Divider orientation="vertical" flexItem/>}
+                >
+                    <Container maxWidth={false} sx={{height: '91vh', overflow: 'auto'}}>
+                        <Schedule schedule={schedule} addClass={addClass} removeClass={removeClass} />
+                    </Container>
+                    <Container sx={{paddingY: 2, height: '91vh', overflow: 'auto'}} maxWidth={"xs"}>
+                        <Config classes={selectedClasses} />
+                    </Container>
+                </Stack>
+
+            </div>
         </div>
     )
 }
