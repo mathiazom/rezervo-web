c96c5a0842581875165a740770dc093c9a447a07
Adrian Andersen <adrianandersen@protonmail.com>
2023-04-08 16:09:51 +0200

    feat: update popularity determination

--- lib/popularity.ts
+++ lib/popularity.ts
@@ -4,6 +4,6 @@ import {ClassPopularity} from "../types/derivedTypes";
 export function determineClassPopularity(sitClass: SitClass) {
     if (!sitClass || sitClass.available === undefined) return ClassPopularity.Unknown
     if (sitClass.available <= 0) return ClassPopularity.High
-    if (sitClass.available <= 5) return ClassPopularity.Medium
+    if (sitClass.available / sitClass.capacity >= 0.8) return ClassPopularity.Medium
     return ClassPopularity.Low
 }
