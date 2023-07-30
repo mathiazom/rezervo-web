92d9d3b06b1e99bc5a53dc8cc4cc0588f919f4e9
Adrian Andersen <adrianandersen@protonmail.com>
2023-07-30 08:34:43 +0200

    refactor(Schedule): extract WeekSchedule from Schedule

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -1,10 +1,9 @@
-import React, { Dispatch, SetStateAction, useEffect } from "react";
-import { Box, Divider, Stack } from "@mui/material";
+import React, { Dispatch, SetStateAction } from "react";
+import { Divider } from "@mui/material";
 import { SitClass, SitSchedule } from "../types/sitTypes";
 import { ClassPopularityIndex, AllConfigsIndex } from "../types/rezervoTypes";
 import WeekNavigator from "./WeekNavigator";
-import { useRouter } from "next/router";
-import DaySchedule from "./DaySchedule";
+import WeekSchedule from "./WeekSchedule";
 
 const Schedule = ({
     initialCachedSchedules,
@@ -29,44 +28,19 @@ const Schedule = ({
     // eslint-disable-next-line no-unused-vars
     onInfo: (c: SitClass) => void;
 }) => {
-    const router = useRouter();
-
-    useEffect(() => {
-        const { classId, ...queryWithoutParam } = router.query;
-        if (classId === undefined) {
-            return;
-        }
-        const linkedClass = currentSchedule.days
-            .flatMap((day) => day.classes)
-            .find((_class) => _class.id === Number(classId));
-        if (linkedClass) {
-            onInfo(linkedClass);
-        }
-        router.replace({ query: queryWithoutParam });
-    }, [onInfo, router, currentSchedule.days]);
-
     return (
         <>
             <WeekNavigator initialCachedSchedules={initialCachedSchedules} setCurrentSchedule={setCurrentSchedule} />
             <Divider orientation="horizontal" />
-            <Box sx={{ flexGrow: 1, overflow: "auto" }}>
-                <Stack direction={"column"}>
-                    <Stack direction={"row"} margin={"auto"} spacing={2} px={1}>
-                        {currentSchedule.days.map((day) => (
-                            <DaySchedule
-                                key={day.date}
-                                day={day}
-                                classPopularityIndex={classPopularityIndex}
-                                selectable={selectable}
-                                selectedClassIds={selectedClassIds}
-                                allConfigsIndex={allConfigsIndex}
-                                onSelectedChanged={onSelectedChanged}
-                                onInfo={onInfo}
-                            />
-                        ))}
-                    </Stack>
-                </Stack>
-            </Box>
+            <WeekSchedule
+                currentSchedule={currentSchedule}
+                classPopularityIndex={classPopularityIndex}
+                selectable={selectable}
+                selectedClassIds={selectedClassIds}
+                allConfigsIndex={allConfigsIndex}
+                onSelectedChanged={onSelectedChanged}
+                onInfo={onInfo}
+            />
         </>
     );
 };
