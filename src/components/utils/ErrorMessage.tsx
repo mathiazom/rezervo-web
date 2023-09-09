61ae87b06c8d936f96bb8214718e3b6281b54f03
Mathias Oterhals Myklebust <mathias@oterbust.no>
2023-09-09 11:24:24 +0200

    style(ErrorMessage): sensationalize error box

--- src/components/utils/ErrorMessage.tsx
+++ src/components/utils/ErrorMessage.tsx
@@ -1,15 +1,28 @@
-import { Alert, AlertTitle } from "@mui/material";
+import { Alert, AlertTitle, Box, Typography } from "@mui/material";
 
-import { getErrorMessage } from "@/lib/helpers/errors";
 import { RezervoError } from "@/types/errors";
 import { IntegrationProfile } from "@/types/integration";
 
 function ErrorMessage({ error, integrationProfile }: { error: RezervoError; integrationProfile: IntegrationProfile }) {
     return (
-        <Alert severity={"error"}>
-            <AlertTitle>Noe gikk galt!</AlertTitle>
-            {getErrorMessage(error, integrationProfile)}
-        </Alert>
+        <Box sx={{ display: "flex", justifyContent: "center", bgcolor: "error.main" }}>
+            <Alert
+                severity={"error"}
+                variant={"filled"}
+                sx={{ width: 1388, padding: "1rem 1.75rem 1rem 1.5rem", borderRadius: 0 }}
+            >
+                <AlertTitle>Noe gikk galt</AlertTitle>
+                <Typography>
+                    {error === RezervoError.INTEGRATION_SCHEDULE_UNAVAILABLE ? (
+                        <>
+                            Klarte ikke hente timeplan for <strong>{integrationProfile.name}</strong>
+                        </>
+                    ) : (
+                        <>En ukjent feil har oppstått️</>
+                    )}
+                </Typography>
+            </Alert>
+        </Box>
     );
 }
 
