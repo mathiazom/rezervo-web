f9a9f247b7fec9b364a664779b51083eb22a0218
Adrian Andersen <adrianandersen@protonmail.com>
2023-12-20 23:16:23 +0100

    feat(BookingPopup): display helpful popup when booking is open

---
+++ src/components/modals/BookingPopupModal.tsx
@@ -0,0 +1,98 @@
+import { Add, Clear, HourglassTop } from "@mui/icons-material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Button, Dialog, DialogActions, DialogTitle, Typography } from "@mui/material";
+import DialogContent from "@mui/material/DialogContent";
+import React, { useState } from "react";
+
+import { ChainIdentifier } from "@/lib/activeChains";
+import { useUserSessions } from "@/lib/hooks/useUserSessions";
+import { RezervoClass } from "@/types/chain";
+
+const BookingPopupModal = ({
+    onClose,
+    chain,
+    _class,
+    action,
+}: {
+    onClose: () => void;
+    chain: ChainIdentifier;
+    _class: RezervoClass | null;
+    action: "book" | "cancel" | null;
+}) => {
+    const { mutateSessionsIndex } = useUserSessions(chain);
+    const [bookingLoading, setBookingLoading] = useState(false);
+    const isCancellation = action === "cancel";
+    const classDescription = `${_class?.activity.name} (${_class?.startTime.toFormat("DD kl. HH:mm")})`;
+
+    async function book() {
+        setBookingLoading(true);
+        await fetch(`/api/${chain}/book`, {
+            method: "POST",
+            body: JSON.stringify({ class_id: _class?.id.toString() }, null, 2),
+        });
+        await mutateSessionsIndex();
+        setBookingLoading(false);
+        onClose();
+    }
+
+    async function cancelBooking() {
+        setBookingLoading(true);
+        await fetch(`/api/${chain}/cancel-booking`, {
+            method: "POST",
+            body: JSON.stringify({ class_id: _class?.id.toString() }, null, 2),
+        });
+        await mutateSessionsIndex();
+        setBookingLoading(false);
+        onClose();
+    }
+
+    return (
+        <Dialog
+            open={action !== null}
+            onClose={() => {
+                if (!bookingLoading) {
+                    onClose();
+                }
+            }}
+            maxWidth={"xs"}
+            fullWidth={true}
+        >
+            {_class !== null && (
+                <>
+                    <DialogTitle>
+                        {isCancellation ? "Vil du avbestille timen?" : "Vil du booke førstkommende time?"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography>
+                            {isCancellation
+                                ? `Du har allerede booket ${classDescription}. Vil du avbestille denne nå?`
+                                : `Booking for ${classDescription} har allerede åpnet. Vil du booke nå?`}
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button disabled={bookingLoading} onClick={onClose} variant={"outlined"} color={"inherit"}>
+                            Lukk
+                        </Button>
+                        <LoadingButton
+                            startIcon={
+                                isCancellation ? <Clear /> : _class.availableSlots > 0 ? <Add /> : <HourglassTop />
+                            }
+                            color={isCancellation ? "error" : _class.availableSlots > 0 ? "primary" : "warning"}
+                            variant={"outlined"}
+                            onClick={isCancellation ? cancelBooking : book}
+                            loading={bookingLoading}
+                        >
+                            {isCancellation
+                                ? "Avbestill"
+                                : _class.availableSlots > 0
+                                ? "Book nå"
+                                : "Sett meg på venteliste"}
+                        </LoadingButton>
+                    </DialogActions>
+                </>
+            )}
+        </Dialog>
+    );
+};
+
+export default BookingPopupModal;
