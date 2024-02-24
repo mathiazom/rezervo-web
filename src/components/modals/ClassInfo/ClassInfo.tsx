mathiazom/rezervo-web

commit 8713bebc8e703a76c65ed26eb9678a8e85f3739d

feat(ClassCard/ClassInfo): no-show badges

src/components/modals/ClassInfo/ClassInfo.tsx
@@ -21,16 +21,17 @@ import Button from "@mui/material/Button";
 import Image from "next/image";
 import React, { useState } from "react";
 
+import ClassInfoUsersGroup from "@/components/modals/ClassInfo/ClassInfoUsersGroup";
 import ClassPopularityMeter from "@/components/schedule/class/ClassPopularityMeter";
-import ClassUsersAvatarGroup from "@/components/schedule/class/ClassUsersAvatarGroup";
 import ConfirmationDialog from "@/components/utils/ConfirmationDialog";
+import { NoShowBadgeIcon } from "@/components/utils/NoShowBadgeIcon";
+import { PlannedNotBookedBadgeIcon } from "@/components/utils/PlannedNotBookedBadgeIcon";
 import { ChainIdentifier } from "@/lib/activeChains";
 import { isClassInThePast } from "@/lib/helpers/date";
 import { stringifyClassPopularity } from "@/lib/helpers/popularity";
 import { classConfigRecurrentId, classRecurrentId } from "@/lib/helpers/recurrentId";
 import { useUserConfig } from "@/lib/hooks/useUserConfig";
 import { useUserSessions } from "@/lib/hooks/useUserSessions";
-import { formatNameArray } from "@/lib/utils/arrayUtils";
 import { hexWithOpacityToRgb } from "@/lib/utils/colorUtils";
 import { RezervoClass } from "@/types/chain";
 import { ClassPopularity } from "@/types/popularity";
@@ -69,9 +70,15 @@ export default function ClassInfo({
 
     const selfOnWaitlist = usersOnWaitlist.some((u) => u.is_self);
 
+    const noShowUsers = userSessions.filter(({ status }) => status === SessionStatus.NOSHOW);
+
+    const selfNoShow = noShowUsers.some((u) => u.is_self);
+
     const usersPlanned = configUsers.filter(
         ({ user_name }) => !userSessions.map((u) => u.user_name).includes(user_name),
     );
+
+    const selfPlanned = usersPlanned.some((u) => u.is_self);
 
     const classInUserConfig = userConfig?.recurringBookings
         ?.map(classConfigRecurrentId)
@@ -266,88 +273,48 @@ export default function ClassInfo({
                     </Typography>
                 </Box>
             )}
-            {!isInThePast && usersPlanned.length > 0 && (
-                <Box sx={{ display: "flex", alignItems: "center", gap: 1.5, mt: 1.5 }}>
-                    <ClassUsersAvatarGroup
-                        users={usersPlanned.map((u) => u.user_name)}
-                        alert={_class.isBookable}
-                        loading={userSessionsLoading}
-                    />
-                    <Typography variant="body2" color={userSessionsLoading ? "text.disabled" : "text.secondary"}>
-                        {userSessionsLoading
-                            ? "henter bookingstatus ..."
-                            : `${formatNameArray(
-                                  usersPlanned.filter((u) => !u.is_self).map((u) => u.user_name),
-                                  4,
-                                  usersPlanned.some((u) => u.is_self),
-                              )} ${
-                                  _class.isBookable
-                                      ? "har planlagt denne timen, men ikke booket plass!"
-                                      : _class.isCancelled
-                                      ? "skulle på denne timen"
-                                      : "skal på denne timen"
-                              }`}
-                    </Typography>
-                </Box>
+            <ClassInfoUsersGroup
+                users={usersPlanned}
+                includeSelf={selfPlanned}
+                badgeIcon={_class.isBookable ? <PlannedNotBookedBadgeIcon /> : undefined}
+                loading={userSessionsLoading}
+                text={
+                    _class.isBookable
+                        ? "har planlagt denne timen, men ikke booket plass!"
+                        : _class.isCancelled
+                        ? "skulle på denne timen"
+                        : "skal på denne timen"
+                }
+            />
+            {!isInThePast && (
+                <ClassInfoUsersGroup
+                    users={usersOnWaitlist}
+                    includeSelf={selfOnWaitlist}
+                    rippleColor={StatusColors.WAITLIST}
+                    isCancelled={_class.isCancelled}
+                    text={_class.isCancelled ? "var på venteliste for denne timen" : "er på venteliste for denne timen"}
+                />
             )}
-            {!isInThePast && usersOnWaitlist.length > 0 && (
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: 1.25,
-                        mt: 1.5,
-                        opacity: _class.isCancelled ? cancelledOpacity : 1,
-                    }}
-                >
-                    <ClassUsersAvatarGroup
-                        users={usersOnWaitlist.map((u) => u.user_name)}
-                        rippleColor={StatusColors.WAITLIST}
-                        invisibleBadges={isInThePast}
-                    />
-                    <Typography variant="body2" color="text.secondary">
-                        {`${formatNameArray(
-                            usersOnWaitlist.filter((u) => !u.is_self).map((u) => u.user_name),
-                            4,
-                            usersOnWaitlist.some((u) => u.is_self),
-                        )} ${
-                            _class.isCancelled
-                                ? "var på venteliste for denne timen"
-                                : "er på venteliste for denne timen"
-                        }`}
-                    </Typography>
-                </Box>
-            )}
-            {usersBooked.length > 0 && (
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: 1.25,
-                        mt: 1.5,
-                        opacity: _class.isCancelled ? cancelledOpacity : 1,
-                    }}
-                >
-                    <ClassUsersAvatarGroup
-                        users={usersBooked.map((u) => u.user_name)}
-                        rippleColor={StatusColors.ACTIVE}
-                        invisibleBadges={isInThePast}
-                    />
-                    <Typography variant="body2" color="text.secondary">
-                        {`${formatNameArray(
-                            usersBooked.filter((u) => !u.is_self).map((u) => u.user_name),
-                            4,
-                            selfBooked,
-                        )} ${
-                            _class.isCancelled
-                                ? "hadde booket denne timen"
-                                : isInThePast
-                                ? "var på denne timen"
-                                : "har booket denne timen"
-                        }`}
-                    </Typography>
-                </Box>
-            )}
+            <ClassInfoUsersGroup
+                users={usersBooked}
+                includeSelf={selfBooked}
+                rippleColor={StatusColors.ACTIVE}
+                invisibleBadges={isInThePast}
+                isCancelled={_class.isCancelled}
+                text={
+                    _class.isCancelled
+                        ? "hadde booket denne timen"
+                        : isInThePast
+                        ? "var på denne timen"
+                        : "har booket denne timen"
+                }
+            />
+            <ClassInfoUsersGroup
+                users={noShowUsers}
+                includeSelf={selfNoShow}
+                badgeIcon={<NoShowBadgeIcon />}
+                text={"booket plass, men møtte ikke opp!"}
+            />
             {user === undefined && !isInThePast && (
                 <Alert severity="info" sx={{ mt: 1.5 }} icon={<Login fontSize={"small"} />}>
                     Du må logge inn for å kunne booke eller legge til timer i timeplanen

src/components/modals/ClassInfo/ClassInfoUsersGroup.tsx
@@ -1,0 +1,59 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+import ClassUsersAvatarGroup from "@/components/schedule/class/ClassUsersAvatarGroup";
+import { formatNameArray } from "@/lib/utils/arrayUtils";
+import { UserNameWithIsSelf } from "@/types/config";
+
+export default function ClassInfoUsersGroup({
+    users,
+    includeSelf,
+    text,
+    isCancelled = false,
+    invisibleBadges = false,
+    badgeIcon,
+    loading = false,
+    rippleColor,
+}: {
+    users: UserNameWithIsSelf[];
+    includeSelf: boolean;
+    text: string;
+    isCancelled?: boolean;
+    invisibleBadges?: boolean;
+    badgeIcon?: React.ReactNode | undefined;
+    loading?: boolean;
+    rippleColor?: string | undefined;
+}) {
+    const cancelledOpacity = 0.5;
+
+    return (
+        users.length > 0 && (
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: badgeIcon ? 1.5 : 1.25,
+                    mt: 1.5,
+                    opacity: isCancelled ? cancelledOpacity : 1,
+                }}
+            >
+                <ClassUsersAvatarGroup
+                    users={users.map((u) => u.user_name)}
+                    rippleColor={rippleColor}
+                    invisibleBadges={invisibleBadges}
+                    badgeIcon={badgeIcon}
+                    loading={loading}
+                />
+                <Typography variant="body2" color={loading ? "text.disabled" : "text.secondary"}>
+                    {loading
+                        ? "henter bookingstatus ..."
+                        : `${formatNameArray(
+                              users.filter((u) => !u.is_self).map((u) => u.user_name),
+                              4,
+                              includeSelf,
+                          )} ${text}`}
+                </Typography>
+            </Box>
+        )
+    );
+}

src/components/schedule/class/ClassCard.tsx
@@ -1,18 +1,19 @@
 import { CancelRounded, EventBusy, EventRepeat } from "@mui/icons-material";
-import { Avatar, AvatarGroup, Badge, Box, Card, CardActions, CardContent, Tooltip, Typography } from "@mui/material";
+import { AvatarGroup, Badge, Box, Card, CardActions, CardContent, Tooltip, Typography } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 import ClassPopularityMeter from "@/components/schedule/class/ClassPopularityMeter";
 import ClassUserAvatar from "@/components/schedule/class/ClassUserAvatar";
-import RippleBadge from "@/components/utils/RippleBadge";
+import { NoShowBadgeIcon } from "@/components/utils/NoShowBadgeIcon";
+import { PlannedNotBookedBadgeIcon } from "@/components/utils/PlannedNotBookedBadgeIcon";
 import { ChainIdentifier } from "@/lib/activeChains";
 import { isClassInThePast } from "@/lib/helpers/date";
 import { classRecurrentId } from "@/lib/helpers/recurrentId";
 import { useUserConfig } from "@/lib/hooks/useUserConfig";
 import { useUserSessions } from "@/lib/hooks/useUserSessions";
 import { randomElementFromArray } from "@/lib/utils/arrayUtils";
-import { hexColorHash, hexWithOpacityToRgb } from "@/lib/utils/colorUtils";
+import { hexWithOpacityToRgb } from "@/lib/utils/colorUtils";
 import { EnterLeaveAnimation, OVER_THE_TOP_ANIMATIONS } from "@/types/animation";
 import { RezervoClass } from "@/types/chain";
 import { ClassPopularity } from "@/types/popularity";
@@ -188,40 +189,34 @@ const ClassCard = ({
                                                 <ClassUserAvatar
                                                     key={user_name}
                                                     username={user_name}
-                                                    alert={_class.isBookable}
+                                                    badgeIcon={
+                                                        _class.isBookable ? <PlannedNotBookedBadgeIcon /> : undefined
+                                                    }
                                                     loading={userSessionsLoading}
                                                 />
                                             ))}
                                         {userSessions.length > 0 &&
-                                            userSessions.map(({ user_name, status }) => {
-                                                const rippleColor =
-                                                    status === SessionStatus.BOOKED ||
-                                                    status === SessionStatus.CONFIRMED
-                                                        ? StatusColors.ACTIVE
-                                                        : StatusColors.WAITLIST;
-                                                return (
-                                                    <RippleBadge
-                                                        key={user_name}
-                                                        invisible={isInThePast}
-                                                        overlap="circular"
-                                                        anchorOrigin={{
-                                                            vertical: "bottom",
-                                                            horizontal: "right",
-                                                        }}
-                                                        variant={"dot"}
-                                                        rippleColor={rippleColor}
-                                                    >
-                                                        <Avatar
-                                                            alt={user_name}
-                                                            sx={{
-                                                                backgroundColor: hexColorHash(user_name),
-                                                            }}
-                                                        >
-                                                            {user_name[0]}
-                                                        </Avatar>
-                                                    </RippleBadge>
-                                                );
-                                            })}
+                                            userSessions.map(({ user_name, status }) => (
+                                                <Fragment key={user_name}>
+                                                    <ClassUserAvatar
+                                                        username={user_name}
+                                                        invisibleBadge={isInThePast}
+                                                        badgeIcon={
+                                                            status === SessionStatus.NOSHOW ? (
+                                                                <NoShowBadgeIcon />
+                                                            ) : undefined
+                                                        }
+                                                        rippleColor={
+                                                            status === SessionStatus.BOOKED ||
+                                                            status === SessionStatus.CONFIRMED
+                                                                ? StatusColors.ACTIVE
+                                                                : status === SessionStatus.WAITLIST
+                                                                ? StatusColors.WAITLIST
+                                                                : undefined
+                                                        }
+                                                    />
+                                                </Fragment>
+                                            ))}
                                     </AvatarGroup>
                                 )}
                             </Box>

src/components/schedule/class/ClassUserAvatar.tsx
@@ -1,5 +1,4 @@
-import ErrorRoundedIcon from "@mui/icons-material/ErrorRounded";
-import { Avatar, Badge, CircularProgress, circularProgressClasses, useTheme } from "@mui/material";
+import { Avatar, Badge, CircularProgress, circularProgressClasses } from "@mui/material";
 import React from "react";
 
 import RippleBadge from "@/components/utils/RippleBadge";
@@ -10,16 +9,15 @@ export default function ClassUserAvatar({
     username,
     rippleColor,
     invisibleBadge,
-    alert = false,
+    badgeIcon,
     loading = false,
 }: {
     username: string;
     rippleColor?: string | undefined;
-    alert?: boolean;
+    badgeIcon?: React.ReactNode | undefined;
     invisibleBadge?: boolean;
     loading?: boolean;
 }) {
-    const theme = useTheme();
     const avatar = (
         <Avatar
             alt={username}
@@ -30,7 +28,7 @@ export default function ClassUserAvatar({
             {username[0]}
         </Avatar>
     );
-    return alert || loading ? (
+    return badgeIcon || loading ? (
         <Badge
             overlap="circular"
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
@@ -61,19 +59,7 @@ export default function ClassUserAvatar({
                         thickness={10}
                     />
                 ) : (
-                    alert && (
-                        <ErrorRoundedIcon
-                            fontSize={"small"}
-                            color={"error"}
-                            sx={{
-                                backgroundColor: theme.palette.background.default,
-                                borderRadius: "50%",
-                                fontSize: "1.1rem",
-                                marginTop: "-0.2rem",
-                                marginLeft: "-0.2rem",
-                            }}
-                        />
-                    )
+                    badgeIcon
                 )
             }
         >

src/components/schedule/class/ClassUsersAvatarGroup.tsx
@@ -6,13 +6,13 @@ import ClassUserAvatar from "@/components/schedule/class/ClassUserAvatar";
 export default function ClassUsersAvatarGroup({
     users,
     rippleColor,
-    alert = false,
+    badgeIcon,
     loading = false,
     invisibleBadges = false,
 }: {
     users: string[];
     rippleColor?: string | undefined;
-    alert?: boolean;
+    badgeIcon?: React.ReactNode | undefined;
     loading?: boolean;
     invisibleBadges?: boolean;
 }) {
@@ -39,7 +39,7 @@ export default function ClassUsersAvatarGroup({
                     username={username}
                     rippleColor={rippleColor}
                     invisibleBadge={invisibleBadges}
-                    alert={alert}
+                    badgeIcon={badgeIcon}
                     loading={loading}
                 />
             ))}

src/components/utils/NoShowBadgeIcon.tsx
@@ -1,0 +1,22 @@
+import GavelRoundedIcon from "@mui/icons-material/GavelRounded";
+import { useTheme } from "@mui/material";
+import React from "react";
+
+export function NoShowBadgeIcon() {
+    const theme = useTheme();
+    return (
+        <GavelRoundedIcon
+            fontSize={"small"}
+            color={"error"}
+            sx={{
+                backgroundColor: theme.palette.background.default,
+                borderRadius: "50%",
+                border: "0.1rem solid red",
+                fontSize: "1.1rem",
+                padding: "0.15rem",
+                marginTop: "-0.2rem",
+                marginLeft: "-0.2rem",
+            }}
+        />
+    );
+}

src/components/utils/PlannedNotBookedBadgeIcon.tsx
@@ -1,0 +1,19 @@
+import ErrorRoundedIcon from "@mui/icons-material/ErrorRounded";
+import { useTheme } from "@mui/material";
+
+export function PlannedNotBookedBadgeIcon() {
+    const theme = useTheme();
+    return (
+        <ErrorRoundedIcon
+            fontSize={"small"}
+            color={"error"}
+            sx={{
+                backgroundColor: theme.palette.background.default,
+                borderRadius: "50%",
+                fontSize: "1.1rem",
+                marginTop: "-0.2rem",
+                marginLeft: "-0.2rem",
+            }}
+        />
+    );
+}

src/types/userSessions.ts
@@ -1,6 +1,9 @@
+import { UserNameWithIsSelf } from "@/types/config";
+
 export enum SessionStatus {
     BOOKED = "BOOKED",
     CONFIRMED = "CONFIRMED",
+    NOSHOW = "NOSHOW",
     WAITLIST = "WAITLIST",
     UNKNOWN = "UNKNOWN",
 }
@@ -11,9 +14,7 @@ export enum StatusColors {
     UNKNOWN = "#000",
 }
 
-export type UserNameSessionStatus = {
-    is_self: boolean;
-    user_name: string;
+export type UserNameSessionStatus = UserNameWithIsSelf & {
     status: SessionStatus;
 };
 export type UserSessionsIndex = {

commit e373394f4728a0c29772ceba7423b2f839806d60

fix(ClassInfo): don't show planned sessions for past classes
regression introduced in 8713bebc

src/components/modals/ClassInfo/ClassInfo.tsx
@@ -260,27 +260,33 @@ export default function ClassInfo({
                     </Typography>
                 </Box>
             )}
-            <ClassInfoUsersGroup
-                users={usersPlanned}
-                includeSelf={selfPlanned}
-                badgeIcon={_class.isBookable ? <PlannedNotBookedBadgeIcon /> : undefined}
-                loading={userSessionsLoading}
-                text={
-                    _class.isBookable
-                        ? "har planlagt denne timen, men ikke booket plass!"
-                        : _class.isCancelled
-                        ? "skulle på denne timen"
-                        : "skal på denne timen"
-                }
-            />
             {!isInThePast && (
-                <ClassInfoUsersGroup
-                    users={usersOnWaitlist}
-                    includeSelf={selfOnWaitlist}
-                    rippleColor={StatusColors.WAITLIST}
-                    isCancelled={_class.isCancelled}
-                    text={_class.isCancelled ? "var på venteliste for denne timen" : "er på venteliste for denne timen"}
-                />
+                <>
+                    <ClassInfoUsersGroup
+                        users={usersPlanned}
+                        includeSelf={selfPlanned}
+                        badgeIcon={_class.isBookable ? <PlannedNotBookedBadgeIcon /> : undefined}
+                        loading={userSessionsLoading}
+                        text={
+                            _class.isBookable
+                                ? "har planlagt denne timen, men ikke booket plass!"
+                                : _class.isCancelled
+                                ? "skulle på denne timen"
+                                : "skal på denne timen"
+                        }
+                    />
+                    <ClassInfoUsersGroup
+                        users={usersOnWaitlist}
+                        includeSelf={selfOnWaitlist}
+                        rippleColor={StatusColors.WAITLIST}
+                        isCancelled={_class.isCancelled}
+                        text={
+                            _class.isCancelled
+                                ? "var på venteliste for denne timen"
+                                : "er på venteliste for denne timen"
+                        }
+                    />
+                </>
             )}
             <ClassInfoUsersGroup
                 users={usersBooked}