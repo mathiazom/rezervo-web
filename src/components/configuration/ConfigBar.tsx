5004401dbba5811b01080f5008dff54eaaf4242c
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-03-02 17:50:40 +0100

    refactor(ConfigBar): simplify bookingPaused calculation

--- src/components/configuration/ConfigBar.tsx
+++ src/components/configuration/ConfigBar.tsx
@@ -8,7 +8,7 @@ import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
 import { Avatar, Badge, Box, Tooltip, useTheme } from "@mui/material";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo } from "react";
 
 import { useCommunity } from "@/lib/hooks/useCommunity";
 import { hexColorHash } from "@/lib/utils/colorUtils";
@@ -39,15 +39,11 @@ function ConfigBar({
     const { community } = useCommunity();
     const friendRequestCount =
         community?.users.filter((cu) => cu.relationship === UserRelationship.REQUEST_RECEIVED).length ?? 0;
-    const [bookingPaused, setBookingPaused] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (chainConfigs !== null) {
-            const inactiveChains = Object.keys(chainConfigs).filter(
-                (chain) => !chainConfigs[chain as ChainIdentifier]?.active,
-            ) as ChainIdentifier[];
-            setBookingPaused(inactiveChains.length > 0 && Object.keys(chainConfigs).length === inactiveChains.length);
-        }
+    const bookingPaused = useMemo(() => {
+        if (chainConfigs == null) return false;
+        const configsArray = Object.values(chainConfigs);
+        return configsArray.length > 0 && configsArray.every((config) => !config.active);
     }, [chainConfigs]);
 
     useEffect(() => {
@@ -57,122 +53,128 @@ function ConfigBar({
     }, []);
 
     return (
-        <>
-            {!isLoading && !user && (
-                <Button endIcon={<LoginIcon />} href={"/api/auth/login"}>
-                    Logg inn
-                </Button>
-            )}
-            {!isLoading && user && (
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: { xs: 1, md: 1.5 },
-                    }}
-                >
-                    {!isLoadingConfig && isConfigError && (
-                        <Box mr={1.5}>
-                            <Tooltip title={"Feilet"}>
-                                <Badge
-                                    overlap={"circular"}
-                                    badgeContent={<ErrorRoundedIcon fontSize={"small"} color={"error"} />}
+        !isLoading && (
+            <>
+                {user == undefined ? (
+                    <Button endIcon={<LoginIcon />} href={"/api/auth/login"}>
+                        Logg inn
+                    </Button>
+                ) : (
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: { xs: 1, md: 1.5 },
+                        }}
+                    >
+                        {isConfigError ? (
+                            !isLoadingConfig && (
+                                <Box mr={1.5}>
+                                    <Tooltip title={"Feilet"}>
+                                        <Badge
+                                            overlap={"circular"}
+                                            badgeContent={<ErrorRoundedIcon fontSize={"small"} color={"error"} />}
+                                        >
+                                            <CloudOffRoundedIcon color={"disabled"} />
+                                        </Badge>
+                                    </Tooltip>
+                                </Box>
+                            )
+                        ) : (
+                            <>
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                    }}
                                 >
-                                    <CloudOffRoundedIcon color={"disabled"} />
-                                </Badge>
-                            </Tooltip>
-                        </Box>
-                    )}
-                    {!isConfigError && (
-                        <>
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <Tooltip
-                                    title={
-                                        friendRequestCount > 0
-                                            ? `Du har ${friendRequestCount} ubesvarte venneforespørsler`
-                                            : "Venner"
-                                    }
-                                >
-                                    <IconButton onClick={() => onCommunityOpen()}>
-                                        {friendRequestCount > 0 ? (
-                                            <Badge
-                                                badgeContent={friendRequestCount}
-                                                color={"error"}
-                                                sx={{
-                                                    "& .MuiBadge-badge": {
-                                                        fontSize: 10,
-                                                        height: 18,
-                                                        minWidth: 18,
-                                                    },
-                                                }}
-                                            >
-                                                <People />
-                                            </Badge>
-                                        ) : (
-                                            <People />
-                                        )}
-                                    </IconButton>
-                                </Tooltip>
-                                <Tooltip title={`Min timeplan${bookingPaused ? " (pauset)" : ""}`}>
-                                    <Badge
-                                        onClick={() => onAgendaOpen()}
-                                        invisible={!bookingPaused}
-                                        overlap={"circular"}
-                                        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-                                        badgeContent={
-                                            <PauseCircleRounded
-                                                fontSize={"small"}
-                                                color={"disabled"}
-                                                sx={{
-                                                    cursor: "pointer",
-                                                    backgroundColor: theme.palette.background.default,
-                                                    borderRadius: "50%",
-                                                    marginTop: "-0.5rem",
-                                                    marginLeft: "-0.5rem",
-                                                }}
-                                            />
+                                    <Tooltip
+                                        title={
+                                            friendRequestCount > 0
+                                                ? `Du har ${friendRequestCount} ubesvarte venneforespørsler`
+                                                : "Venner"
                                         }
                                     >
-                                        <IconButton onClick={() => onAgendaOpen()}>
-                                            {(userSessions?.length ?? 0) > 0 ? <CalendarMonth /> : <CalendarToday />}
+                                        <IconButton onClick={() => onCommunityOpen()}>
+                                            {friendRequestCount > 0 ? (
+                                                <Badge
+                                                    badgeContent={friendRequestCount}
+                                                    color={"error"}
+                                                    sx={{
+                                                        "& .MuiBadge-badge": {
+                                                            fontSize: 10,
+                                                            height: 18,
+                                                            minWidth: 18,
+                                                        },
+                                                    }}
+                                                >
+                                                    <People />
+                                                </Badge>
+                                            ) : (
+                                                <People />
+                                            )}
                                         </IconButton>
-                                    </Badge>
-                                </Tooltip>
-                                <Tooltip title={"Innstillinger"}>
-                                    <IconButton onClick={() => onSettingsOpen()}>
-                                        <SettingsRoundedIcon />
-                                    </IconButton>
-                                </Tooltip>
-                            </Box>
-                        </>
-                    )}
-                    {user.name && (
-                        <Tooltip title={user.name}>
-                            <Avatar
-                                sx={{
-                                    width: 32,
-                                    height: 32,
-                                    fontSize: 18,
-                                    backgroundColor: hexColorHash(user.name),
-                                }}
-                            >
-                                {user.name[0]}
-                            </Avatar>
+                                    </Tooltip>
+                                    <Tooltip title={`Min timeplan${bookingPaused ? " (pauset)" : ""}`}>
+                                        <Badge
+                                            onClick={() => onAgendaOpen()}
+                                            invisible={!bookingPaused}
+                                            overlap={"circular"}
+                                            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                                            badgeContent={
+                                                <PauseCircleRounded
+                                                    fontSize={"small"}
+                                                    color={"disabled"}
+                                                    sx={{
+                                                        cursor: "pointer",
+                                                        backgroundColor: theme.palette.background.default,
+                                                        borderRadius: "50%",
+                                                        marginTop: "-0.5rem",
+                                                        marginLeft: "-0.5rem",
+                                                    }}
+                                                />
+                                            }
+                                        >
+                                            <IconButton onClick={() => onAgendaOpen()}>
+                                                {(userSessions?.length ?? 0) > 0 ? (
+                                                    <CalendarMonth />
+                                                ) : (
+                                                    <CalendarToday />
+                                                )}
+                                            </IconButton>
+                                        </Badge>
+                                    </Tooltip>
+                                    <Tooltip title={"Innstillinger"}>
+                                        <IconButton onClick={() => onSettingsOpen()}>
+                                            <SettingsRoundedIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                </Box>
+                            </>
+                        )}
+                        {user.name && (
+                            <Tooltip title={user.name}>
+                                <Avatar
+                                    sx={{
+                                        width: 32,
+                                        height: 32,
+                                        fontSize: 18,
+                                        backgroundColor: hexColorHash(user.name),
+                                    }}
+                                >
+                                    {user.name[0]}
+                                </Avatar>
+                            </Tooltip>
+                        )}
+                        <Tooltip title={"Logg ut"}>
+                            <IconButton href={"/api/auth/logout"}>
+                                <LogoutRoundedIcon />
+                            </IconButton>
                         </Tooltip>
-                    )}
-                    <Tooltip title={"Logg ut"}>
-                        <IconButton href={"/api/auth/logout"}>
-                            <LogoutRoundedIcon />
-                        </IconButton>
-                    </Tooltip>
-                </Box>
-            )}
-        </>
+                    </Box>
+                )}
+            </>
+        )
     );
 }
 
