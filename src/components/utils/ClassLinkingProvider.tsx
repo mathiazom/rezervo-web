61447ef89478607eab15f01706906e15b7a77247
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-03-03 12:26:36 +0100

    refactor(ClassLinkingProvider): use useEffect

--- src/components/utils/ClassLinkingProvider.tsx
+++ src/components/utils/ClassLinkingProvider.tsx
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 import { LocalizedDateTime, weekOffsetBetweenDates } from "@/lib/helpers/date";
 import { RezervoClass } from "@/types/chain";
@@ -19,24 +19,29 @@ function ClassLinkingProvider({
     const searchParams = useSearchParams();
     const [checkedWeekOffset, setCheckedWeekOffset] = useState(false);
 
-    const classIdParam = searchParams?.get("classId") ?? null;
-    const startTimeParam = searchParams?.get("startTime") ?? null;
-
-    if (!checkedWeekOffset && classIdParam !== null && startTimeParam !== null) {
-        setCheckedWeekOffset(true);
-        const startTimeDate = DateTime.fromISO(startTimeParam);
-        setWeekOffset(weekOffsetBetweenDates(startTimeDate, LocalizedDateTime.now()));
-        router.replace(`${pathname}?classId=${classIdParam}`);
-    }
-
-    if (checkedWeekOffset && startTimeParam === null) {
-        setCheckedWeekOffset(false);
-        const linkedClass = classes.find((_class) => _class.id === classIdParam);
-        if (linkedClass) {
-            setClassInfoClass(linkedClass);
+    useEffect(() => {
+        const classIdParam = searchParams?.get("classId") ?? null;
+        const startTimeParam = searchParams?.get("startTime") ?? null;
+        if (pathname == null || classIdParam === null) {
+            return;
         }
-        router.replace(`${pathname}`);
-    }
+        if (!checkedWeekOffset) {
+            if (startTimeParam !== null) {
+                setCheckedWeekOffset(true);
+                const startTimeDate = DateTime.fromISO(startTimeParam);
+                setWeekOffset(weekOffsetBetweenDates(startTimeDate, LocalizedDateTime.now()));
+                router.replace(`${pathname}?classId=${classIdParam}`);
+                return;
+            }
+        } else if (startTimeParam === null) {
+            const linkedClass = classes.find((_class) => _class.id === classIdParam);
+            if (linkedClass) {
+                setClassInfoClass(linkedClass);
+                setCheckedWeekOffset(false);
+                router.replace(pathname);
+            }
+        }
+    }, [searchParams, checkedWeekOffset, classes, pathname, router, setWeekOffset, setClassInfoClass]);
 
     return <></>;
 }
