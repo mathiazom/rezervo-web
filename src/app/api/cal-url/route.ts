2215027bfc778a489e9b1f9976f7aa859c6ab0ed
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-03-09 14:36:28 +0100

    fix(api): use public url for calendar feed

--- src/app/api/cal-url/route.ts
+++ src/app/api/cal-url/route.ts
@@ -1,6 +1,6 @@
 import { withApiAuthRequired } from "@auth0/nextjs-auth0";
 
-import { buildBackendPath, doOperation, get, respondUnauthorized, tryUseRefreshToken } from "@/lib/helpers/api";
+import { doOperation, get, respondUnauthorized, tryUseRefreshToken } from "@/lib/helpers/api";
 
 export const GET = withApiAuthRequired(async (req) => {
     const accessToken = await tryUseRefreshToken(req);
@@ -9,7 +9,7 @@ export const GET = withApiAuthRequired(async (req) => {
     if (!response.ok) return response;
 
     const calendarToken = await response.json();
-    const calendarFeedUrl = new URL(buildBackendPath("cal"));
+    const calendarFeedUrl = new URL(`${process.env["NEXT_PUBLIC_CONFIG_HOST"]}/cal`);
     const includePastQuery = new URL(req.url ?? "").searchParams.get("include_past") ?? "";
     calendarFeedUrl.searchParams.set("include_past", includePastQuery === "true" ? "true" : "false");
     calendarFeedUrl.searchParams.set("token", calendarToken);
