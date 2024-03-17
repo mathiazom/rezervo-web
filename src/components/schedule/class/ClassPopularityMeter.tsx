b9db0f386668557fecc974d04339f7eeff61bfb4
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-03-17 20:22:21 +0100

    (WIP) fix: outlined popularity meter

--- src/components/schedule/class/ClassPopularityMeter.tsx
+++ src/components/schedule/class/ClassPopularityMeter.tsx
@@ -1,39 +1,43 @@
 import QuestionMarkRoundedIcon from "@mui/icons-material/QuestionMarkRounded";
 import SpeedRoundedIcon from "@mui/icons-material/SpeedRounded";
-import { Tooltip } from "@mui/material";
+import {Tooltip} from "@mui/material";
 import React from "react";
 
 import RippleBadge from "@/components/utils/RippleBadge";
-import { isClassInThePast } from "@/lib/helpers/date";
-import { determineClassPopularity, stringifyClassPopularity } from "@/lib/helpers/popularity";
-import { RezervoClass } from "@/types/chain";
-import { ClassPopularity } from "@/types/popularity";
-import { StatusColors } from "@/types/userSessions";
+import {isClassInThePast} from "@/lib/helpers/date";
+import {determineClassPopularity, stringifyClassPopularity} from "@/lib/helpers/popularity";
+import {RezervoClass} from "@/types/chain";
+import {ClassPopularity} from "@/types/popularity";
+import {StatusColors} from "@/types/userSessions";
 
-const ClassPopularityIcon = ({ popularity }: { popularity: ClassPopularity }) => {
-    switch (popularity) {
-        case ClassPopularity.High:
-            return <SpeedRoundedIcon style={{ color: "red" }} />;
-        case ClassPopularity.Medium:
-            return <SpeedRoundedIcon style={{ color: "orange" }} />;
-        case ClassPopularity.Low:
-            return <SpeedRoundedIcon style={{ color: "green" }} />;
-        default:
-            return <QuestionMarkRoundedIcon fontSize={"small"} />;
+const ClassPopularityIcon = ({popularity}: { popularity: ClassPopularity }) => {
+    if (popularity === ClassPopularity.Unknown) {
+        return <QuestionMarkRoundedIcon fontSize={"small"}/>;
     }
+    return <SpeedRoundedIcon style={{
+        color: (
+            popularity === ClassPopularity.High ? "red" : (
+                popularity === ClassPopularity.Medium ? "orange" : "green"
+            )
+        ),
+        filter: "drop-shadow(0 0 2px white)",
+        '[data-mui-color-scheme="dark"] &': {
+            filter: "drop-shadow(0 0 2px #191919)"
+        },
+    }}/>;
 };
 
 const ClassPopularityMeter = ({
-    _class,
-    historicPopularity,
-}: {
+                                  _class,
+                                  historicPopularity,
+                              }: {
     _class: RezervoClass;
     historicPopularity: ClassPopularity;
 }) => {
     const popularity =
         isClassInThePast(_class) || _class.isBookable ? determineClassPopularity(_class) : historicPopularity;
 
-    const popularityIcon = <ClassPopularityIcon popularity={popularity} />;
+    const popularityIcon = <ClassPopularityIcon popularity={popularity}/>;
 
     if (_class.isBookable) {
         return (
